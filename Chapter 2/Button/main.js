8ec7cec4aaa14d788b90ed4f48e47a9aa6d57897
adjohnson23 <alex@latentninja.com>
2023-03-08 16:32:42 -0500

    Added a buttons page.

---
+++ Chapter 2/Button/main.js
@@ -0,0 +1,9 @@
+//Practice javascript
+
+function main() {
+    const btn3 = document.getElementById("button3")
+    btn3.addEventListener("click", function() {
+    alert("Hello Mars!");
+    })
+
+}
